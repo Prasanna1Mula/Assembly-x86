690a56cbea41fba90842876ce36f087a1d2b2328
Prasanna Mula <147959416+Prasanna1Mula@users.noreply.github.com>
2025-04-20 16:24:18 +0530

    loop.asm

--- loop.asm
+++ loop.asm
@@ -3,7 +3,17 @@
 .CODE 
 
 MAIN:
-  MOV CX, 10H
-  MOC BL, '1'
+  MOV CX, 10H  ; Moving 10 to CX register 
+  MOC BL, '1' ; moving 1 to BL resgister 
 
-NEXT
+NEXT:
+  MOV AH, 02H  ; moving next number to AH register 
+  DIV DL, BL
+  INT 21H 
+
+  INC BL ; BL is incremented by 1 
+  LOOP NEXT 
+
+  MOV AH, 4CH 
+  INT 21H 
+END MAIN 
