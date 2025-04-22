a9a6db8334ede58d68a7a1df01487a4e28f22c3b
Prasanna Mula <147959416+Prasanna1Mula@users.noreply.github.com>
2025-04-22 22:10:22 +0530

    ascOrder.asm

--- ascOrder.asm
+++ ascOrder.asm
@@ -16,22 +16,22 @@ MOV SI, 1000H
 MOV CH, 04H ; Intilaise CH register for loop 
 
 UP2 : MOV CL, 04H
-LEA SI, STRING1
+LEA SI, STRING1 ; Loading effective address to the source index 
 
-UP1 : MOV AL, [SI] 
-MOV BL, [SI+1] 
-CMP AL, BL
-JC DOWN 
+UP1 : MOV AL, [SI] ; Contents of source index to store in AL register 
+MOV BL, [SI+1] ; The next level of contents to BL register 
+CMP AL, BL ; Comparing AL and BL 
+JC DOWN ; Lower number moves down after comparing between both 
 MOV DL, [SI+1]
-XCHG [SI], DL
-MOV [SI+1], DL 
+XCHG [SI], DL ; if current element is smaller than the next number 
+MOV [SI+1], DL ; Arranging in ascending order 
 
-DOWN: INC SI
-DEC CL
-JNZ UP1 
-DEC CH 
-JNZ UP2 
+DOWN: INC SI ; SI is increased by one point to next pair of elements in that array 
+DEC CL ; Decrement in loop counter 
+JNZ UP1 ; If it is not zero, jumps to UP1 
+DEC CH ; Decrement in loop counter 
+JNZ UP2 ; If it is not zero jumps to UP2 
 
 
-INT3 
+INT3 ; Terminating preogram 
 CODE ENDS 
