2dc2b80b56647ba0aedb58ffe433013c2427b03c
Prasanna Mula <147959416+Prasanna1Mula@users.noreply.github.com>
2025-04-25 12:54:25 +0530

    desOrder.asm

--- desOrder.asm
+++ desOrder.asm
@@ -7,23 +7,24 @@ STRING DB 99H, 12H, 14H, 45H, 48K
 DATA ENDS 
 ASSUME CS:CODE, DS: DATA
 START : MOV AX, @DATA 
-        MOV DS; AX
-        MOV CH, 04H
-        UP2: MOV CL, 04H
-        LEA SI, STRING1
+        MOV DS, AX         ; Initialise data segment 
+        MOV SI, 1000H      ; Intialise Segment Index with 1000H 
+        MOV CH, 04H        ; Move 04 to outer loop of counter register 
+        UP2: MOV CL, 04H   ; Mov 04H to inner loop of counter register 
+        LEA SI, STRING1    ; load effective address to SI register 
 
-UP1 : MOV AL, [SI]
-      MOV BL, [SI+1]
-      CMP AL, BL
-      JNC DOWN 
-      MOV DL, [SI+1]
-      XCHG [SI],DL
+UP1 : MOV AL, [SI]     ; Load the effective memory address to AL register 
+      MOV BL, [SI+1]   ; Load the effective memory to BL register 
+      CMP AL, BL       ; Comapre between AL and BL registers 
+      JNC DOWN         ; The Higher number moves down 
+      MOV DL, [SI+1]   
+      XCHG [SI],DL     ; Swap if the current element is less than the previous number 
       MOV [SI+1],DL
 
-DOWN: INC SI 
-      DEC CL
+DOWN: INC SI           ; Increments SI register to next pointer 
+      DEC CL           ; Decrements the inner loop counter to UP1 
       JNZ UP1
-      DEC CH
+      DEC CH           ; Derement the outerloop counter to UP2 if CH is not zero 
       JNZ UP2
       INT 3 
       CODE ENDS 
