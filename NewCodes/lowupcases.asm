c7734f394afd2b9ed52fba3b44ded5676df0d91a
Prasanna Mula <147959416+Prasanna1Mula@users.noreply.github.com>
2025-05-06 21:39:45 +0530

    Converting Lower to Upper Case

--- NewCodes/lowupcases.asm
+++ NewCodes/lowupcases.asm
@@ -13,3 +13,37 @@ CODE SEGMENT
 START: 
       MOV AX, @DATA 
       MOV DS, AX                   ; Initialize data segment 
+
+      MOV SI, OFFSET STRING        ; Point SI to starting of the string
+
+CONVERT_LOOP:
+      MOV AL, [SI]                 ; Load the present character 
+      CMP AL, '&'                   ; Check for end of the string 
+      JE DISPLAY                   ; If end of the string 
+
+      ; Check if the character is lower case
+      CMP AL, 'a' 
+      JB NEXT_CHAR                  ; If below 'a', skip 
+      CMP AL, 'z'  
+      JA NEXT_CHAR                  ; If ablove 'z', skip
+
+      ; Convert the character into the uppercase by subtracting 32 (20h) 
+      SUB AL, 20H                   ; Converting the lowercase to uppercase 
+      MOV [SI], AL                  ; Store back the converted character 
+
+
+NEXT_CHAR: 
+      INC SI                       ; Move to next character  
+      JMP CONVERT_LOOP             ; Repeat for the next character 
+
+DISPLAY: 
+      ; Display the converted string 
+      MOV AH, 09h                  ;  Funcion to display the string 
+      MOV DX, OFFSET STRING 
+      INT 21H 
+
+      MOV AH, 4CH 
+      INT 21H 
+
+CODE ENDS 
+END START 
